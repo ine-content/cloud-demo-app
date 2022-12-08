d683720ad6c5f452888d8a9b30b162f752f173e4
Tracy Wallace <tswwci@hotmail.com>
2022-12-08 14:55:25 -0500

    Added queue test.

--- Controllers/PrimaryController.cs
+++ Controllers/PrimaryController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
-
+using Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Threading.Tasks;
+using System;
 namespace csharp_webserver.Controllers
 {
     [Route("api/[controller]")]
@@ -30,6 +34,20 @@ namespace csharp_webserver.Controllers
                 remoteIp = HttpContext.Connection != null ? HttpContext.Connection.RemoteIpAddress.ToString() : "",
                 message = _config.GetValue<string>("message","")
             }); ;
+        }
+
+        [HttpGet("queue"), HttpPost("queue")]
+        public async Task<ActionResult<string>> AddToQueue(int itemCount){
+            string queueConnection = _config.GetValue<string>("queueConnection");
+            string queueName = _config.GetValue<string>("queueName");
+            QueueClient client = new QueueClient(queueConnection,queueName);
+            await client.CreateIfNotExistsAsync();
+            for(int lcv=0;lcv<itemCount;lcv++){
+                await client.SendMessageAsync($"Message {lcv} send at {DateTime.Now.ToLongTimeString()}");
+            }
+
+
+            return new OkObjectResult($"{itemCount} messages sent to queue.");
         }
     }
 
